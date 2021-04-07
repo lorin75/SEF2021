18b37aa622c2c6188d93b05bea9f8f76aea56bb6
Lorin <lorin.rapciuc@ligaac.ro>
2021-04-07 08:30:05 +0300

    adding project structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("welcome to our sef project!\n");//comment
-        System.out.println("i've cloned the project\n");//comment
+        System.out.println("i've cloned the project\n");//niiiiiice
     }
 }
