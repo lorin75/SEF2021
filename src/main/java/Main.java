de8cd6788e71dae044fd0d91397215da168daa85
Lorin <lorin.rapciuc@ligaac.ro>
2021-05-18 21:00:18 +0300

    start page created

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("welcome to our sef project!\n");//comment
-        System.out.println("i've cloned the project\n");//niiiiiice
-    }
-}
